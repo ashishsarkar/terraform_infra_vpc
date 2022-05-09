2b68b7a842e89ae687d2256a0770f77fe01dbd5b
Ashish Sarkar <ashishsarkar2000@gmail.com>
2022-05-09 10:41:21 +0530

    Update terraform_update_code.sh

--- terraform_update_code.sh
+++ terraform_update_code.sh
@@ -1,2 +1,7 @@
+   
 #!/bin/bash
 
+terraform init -backend-config=environments/dev/backend.config
+terraform plan -var-file=environments/dev/variables.tfvars  
+terraform apply -var-file=environments/dev/variables.tfvars  --auto-approve=true
+# terraform destroy -var-file=environments/dev/variables.tfvars  --auto-approve=true
